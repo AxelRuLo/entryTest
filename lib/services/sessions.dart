f34920c5558064162712d74e4e045cb1d069366a
Axel Ruiz L <developeraxrulo@gmail.com>
2022-06-03 11:24:53 -0500

    jala

--- lib/services/sessions.dart
+++ lib/services/sessions.dart
@@ -2,15 +2,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Sessions {
-  
   Future<String> getLogin(String username, String password) async {
     // var header = await getJwt();
     final response = await http.get(
-      Uri.parse('http://107.20.130.8/api/login/acces/${username}/${password}'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      }
-    );
+        Uri.parse(
+            'http://107.20.130.8/api/login/acces/${username}/${password}'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
     print(response.statusCode);
     print(response.body);
 
@@ -24,7 +23,8 @@ class Sessions {
   Future<String> getSignUP(
       String username, String password, String phone) async {
     final response = await http.get(
-      Uri.parse('http://54.221.104.210/api/register/acces/${username}/${password}/${phone}'),
+      Uri.parse(
+          'http://54.221.104.210/api/register/acces/${username}/${password}/${phone}'),
     );
     print(response.statusCode);
     print(response.body);
@@ -35,5 +35,4 @@ class Sessions {
       return "0";
     }
   }
-
 }
